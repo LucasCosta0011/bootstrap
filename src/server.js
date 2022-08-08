23b289ab4e290302000207e10d805c3b9b733866
lucascosta0011 <lucascostadeveloper@gmail.com>
2022-08-08 17:48:25 -0300

    fixed exports

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 const app =  require('./app')
-const fs = require('fs');
-const ytdl = require('ytdl-core');
 const PORTA = process.env.PORT || 3000
-var path = require('path');
 
 app.get('/', function (req, res) {
 
